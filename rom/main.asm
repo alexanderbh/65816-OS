b551b221f9fff7bd678831fec52b40e2d10e370e
Alexander Hansen <alexanderbh@gmail.com>
2021-10-31 20:43:11 +0100

    asm code

---
+++ rom/main.asm
@@ -0,0 +1,42 @@
+.setcpu "65816"
+
+.import __VIA1_START__
+
+VIA1B = __VIA1_START__                          ; VIA PORT B input/output register
+VIA1A = __VIA1_START__ + 1                        ; VIA PORT A input/output register
+VIA1B_DIRECTION = __VIA1_START__ + 2              ; VIA PORT B direction register is $6002
+VIA1A_DIRECTION = __VIA1_START__ + 3
+
+
+.SEGMENT "RAM"
+
+.code
+
+ResetVector:
+
+    LDA #$FF
+    STA VIA1A_DIRECTION
+
+    LDA #$AA
+    STA VIA1A
+
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+
+    LDA #$55
+    STA VIA1A
+
+    JMP ResetVector
+
+.SEGMENT "VECTORS"
+    .word ResetVector
