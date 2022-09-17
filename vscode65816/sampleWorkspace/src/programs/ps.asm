6e5647ea49002a52f8478bc44e9c25d30721985f
Alexander Hansen <alexanderbh@gmail.com>
2022-09-17 20:56:40 +0200

    expand ps

--- vscode65816/sampleWorkspace/src/programs/ps.asm
+++ vscode65816/sampleWorkspace/src/programs/ps.asm
@@ -1,11 +1,72 @@
+.RODATA
+
+Data_ps_header:                    .asciiz "ID PID  ST WPID XC"
+Data_10_spaces:                     .asciiz "          "
 
 .code
 .A8
 .I8
 ExecPs:
     longr
-        jsl Sys_GetPID
-        sta $00
-        lda $00
+        write Data_ps_header
+    shortr
+        ldx #$0
+
+    @loop:
+        lda TaskStatus,x
+        cmp #$00
+        beq @next
+        
+    @print:
+        lda #$0A
+        jsl RA8875_WriteChar
+        txa
+        jsl RA8875_WriteHex
+        lda #' '
+        jsl RA8875_WriteChar
+    longa
+        txy
+        txa
+        asl
+        tax
+        lda TaskStructID,x
         jsl RA8875_WriteHex16
+        tyx
+    shorta
+        lda #' '
+        jsl RA8875_WriteChar
+        lda TaskStatus,x
+        jsl RA8875_WriteHex
+        cmp #TASK_STATUS_WAITING_TASK
+        bne @nowait                           ; skip writing waiting task id
+
+        lda #' '
+        jsl RA8875_WriteChar
+        txy
+        txa
+        asl
+        tax
+    longa
+        lda TaskWaitFor,x
+        jsl RA8875_WriteHex16
+    shorta
+        tyx
+        lda #' '
+        jsl RA8875_WriteChar
+
+        jmp @exitcode
+    @nowait:
+    longr
+        write Data_10_spaces+4
+    shortr
+
+    @exitcode:
+        lda TaskExitCode,X
+        jsl RA8875_WriteHex
+    @next:
+        inx
+        cpx #NUMBER_OF_TASKS
+        beq @done
+        jmp @loop
+    @done:
         jml Sys_Exit
